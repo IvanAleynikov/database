e5a8e75ced811a5a3f148c107a8f4013d0368530
Ivan Aleinikov <alejnikov99.99@mail.ru>
2024-02-08 20:36:06 +0300

    initial commit

---
+++ first work.sql
@@ -0,0 +1,28 @@
+insert into  Genres
+values(1, 'Pop'), (2, 'Rap'), (3, 'Rock'), (4, 'Classic');
+
+insert into  Musicians 
+values(1, 'Eminem'), (2, 'Lady Gaga'), (3, 'Anees'), (4, 'Bach'), (5, 'Linkin Park'), (6, 'oxxxymiron');
+
+insert into  Multigenre 
+values(1, 2, 1), (2, 1, 2), (3, 3, 3), (4, 3, 5), (5, 4, 4), (6, 2, 6);
+
+insert into  Albums 
+values(1, 'The Marshall Mathers LP', '2003.02.10'), (2, 'The Fame', '2010.11.04'), (3, 'Meteora', '2008.12.01'), (4, 'Горгород', '2015.05.18');
+
+insert into  multialbums  
+values(1, 2, 2), (2, 1, 1), (3, 4, 6), (4, 3, 5);
+
+insert into  Playlists 
+values(1, 'The best Rap music', '2020.05.17'), (2, 'Top 100 tracks in the world', '2022.03.30'), (3, 'Popular', '2010.09.13'), (4, 'Music for home', '2018.11.14'), (5, 'Rock music', '2019.06.07');
+
+insert into  Tracks 
+values(1, '8 mile', '4:54', 1), (2, 'Где нас нет', '4:16', 4), (3, 'A place for my head', '3:05', 3), (4, 'Hold my hand', '3:49', 2), (5, 'Moskingbird', '4:18', 1), (6, 'Numb', '3:08', 3);
+
+insert into Tracksplaylists 
+values(1, 2, 1), (2, 1, 2), (3, 4, 3), (4, 3, 5), (5, 5, 4), (6, 6, 5);
+
+
+
+
+
